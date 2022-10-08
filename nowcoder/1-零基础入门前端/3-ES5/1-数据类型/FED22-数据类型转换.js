ec1fb4cf1409f52f5e51a861e16fdd523726f36e
LiChen <2446432344@qq.com>
2022-10-08 16:50:47 +0800

    FED22-数据类型转换

---
+++ nowcoder/1-零基础入门前端/3-ES5/1-数据类型/FED22-数据类型转换.js
@@ -0,0 +1,8 @@
+// 描述
+// 请补全JavaScript函数，要求以字符串的形式返回两个数字参数的拼接结果。
+// 示例：
+// 1. _splice(223,233) -> "223233"
+// 2. _splice(-223,-233) -> "-223-233"
+function _splice(left, right) {
+  return "" + left + right;
+}
