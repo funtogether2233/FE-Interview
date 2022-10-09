3f04f0cd0d0543a861b3bbbb4a2af4f7dae0c3e4
LiChen <2446432344@qq.com>
2022-10-09 17:27:47 +0800

    FED31-对象属性键名

---
+++ nowcoder/1-零基础入门前端/3-ES5/4-内置对象/FED31-对象属性键名.js
@@ -0,0 +1,8 @@
+// 描述
+// 请补全JavaScript函数，要求以数组的形式输出对象各个属性的键名。
+// 示例：
+// 1. _keys({name:'nowcoder',age:7}) -> ['name','age']
+// 注意：只需考虑对象属性均为原始数据类型的情况。
+function _keys(object) {
+  return Object.keys(object);
+}
