41a0e1b8623f1ab1d5bc7595ece24d7e8014a889
LiChen <2446432344@qq.com>
2022-10-09 18:19:08 +0800

    FED61-添加元素

---
+++ nowcoder/1-零基础入门前端/3-ES5/4-内置对象/FED61-添加元素.js
@@ -0,0 +1,10 @@
+// 描述
+// 在数组 arr 开头添加元素 item。不要直接修改数组 arr，结果返回新的数组
+// 示例1
+// 输入：
+// [1, 2, 3, 4], 10
+// 输出：
+// [10, 1, 2, 3, 4]
+function prepend(arr, item) {
+  return [item].concat(arr);
+}
