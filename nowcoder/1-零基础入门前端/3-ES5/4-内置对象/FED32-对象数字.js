75c2e83c4f33a7bb6caec9f194162607305a83de
LiChen <2446432344@qq.com>
2022-10-09 17:29:45 +0800

    FED32-对象数字

---
+++ nowcoder/1-零基础入门前端/3-ES5/4-内置对象/FED32-对象数字.js
@@ -0,0 +1,7 @@
+// 描述
+// 请补全JavaScript函数，要求将数字参数转换为对象的形式返回。
+// 示例：
+// 1. typeof number === 'number' -> typeof _numbertoobject(number) === 'object'
+function _numbertoobject(number) {
+  return Object(number);
+}
