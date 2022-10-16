64c7f2b49de462a3e61185f5ddc3678ec1d3e780
LiChen <2446432344@qq.com>
2022-10-16 16:19:41 +0800

    nowcoder 22-10-16

---
+++ nowcoder/3-JS能力测评/JS54 函数传参.js
@@ -0,0 +1,10 @@
+// 描述
+// 将数组 arr 中的元素作为调用函数 fn 的参数
+// 示例1
+// 输入：
+// function (greeting, name, punctuation) {return greeting + ', ' + name + (punctuation || '!');}, ['Hello', 'Ellie', '!']
+// 输出：
+// Hello, Ellie!
+function argsAsArray(fn, arr) {
+  return fn(...arr);
+}
