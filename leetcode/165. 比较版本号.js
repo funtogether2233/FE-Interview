6d23921b765639ccc4d47f38476b35e43fd86044
LiChen <2446432344@qq.com>
2022-11-07 10:45:17 +0800

    lc 22-11-7

--- leetcode/165. 比较版本号.js
+++ leetcode/165. 比较版本号.js
@@ -4,8 +4,20 @@
  * @return {number}
  */
 var compareVersion = function (version1, version2) {
-  const v1 = version1.split(".");
-  const v2 = version2.split(".");
+  //   const v1 = version1.split(".");
+  //   const v2 = version2.split(".");
+  //   for (let i = 0; i < v1.length || i < v2.length; ++i) {
+  //     const x = i < v1.length ? parseInt(v1[i]) : 0,
+  //       y = i < v2.length ? parseInt(v2[i]) : 0;
+  //     if (x !== y) {
+  //       return x > y ? 1 : -1;
+  //     }
+  //   }
+  //   return 0;
+
+  // 第二次写，parseInt(undefined)输出NaN
+  const v1 = version1.split("."),
+    v2 = version2.split(".");
   for (let i = 0; i < v1.length || i < v2.length; ++i) {
     const x = i < v1.length ? parseInt(v1[i]) : 0,
       y = i < v2.length ? parseInt(v2[i]) : 0;
--- leetcode/88. 合并两个有序数组.js
+++ leetcode/88. 合并两个有序数组.js
@@ -6,12 +6,27 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function (nums1, m, nums2, n) {
-  (m -= 1), (n -= 1);
-  for (let i = m + n + 1; i >= 0; i--) {
+  //   (m -= 1), (n -= 1);
+  //   for (let i = m + n + 1; i >= 0; i--) {
+  //     if (m >= 0 && n >= 0) {
+  //       nums1[i] = nums1[m] > nums2[n] ? nums1[m--] : nums2[n--];
+  //     } else if (n >= 0) {
+  //       for (let j = 0; j <= n; j++) {
+  //         nums1[j] = nums2[j];
+  //       }
+  //       break;
+  //     } else {
+  //       break;
+  //     }
+  //   }
+  //   return nums1;
+  // 第二次写，没写出来，nums1倒着装入就可以了，又抄了一遍
+  --m, --n;
+  for (let i = m + n + 1; i >= 0; --i) {
     if (m >= 0 && n >= 0) {
       nums1[i] = nums1[m] > nums2[n] ? nums1[m--] : nums2[n--];
     } else if (n >= 0) {
-      for (let j = 0; j <= n; j++) {
+      for (let j = 0; j <= n; ++j) {
         nums1[j] = nums2[j];
       }
       break;
