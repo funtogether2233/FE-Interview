34bfbe1a5a55c1bba2c24d3d06f1621dc333e4ca
LiChen <2446432344@qq.com>
2022-11-09 08:11:48 +0800

    lc 22-11-9

--- leetcode/46. 全排列.js
+++ leetcode/46. 全排列.js
@@ -3,9 +3,29 @@
  * @return {number[][]}
  */
 var permute = function (nums) {
+  //   let res = [],
+  //     output = [];
+  //   for (num of nums) {
+  //     output.push(num);
+  //   }
+  //   const backtrack = (n, first) => {
+  //     if (first == n) {
+  //       res.push(Array.from(output));
+  //       return;
+  //     }
+  //     for (let i = first; i < n; i++) {
+  //       [output[first], output[i]] = [output[i], output[first]];
+  //       backtrack(n, first + 1);
+  //       [output[first], output[i]] = [output[i], output[first]];
+  //     }
+  //   };
+  //   backtrack(nums.length, 0);
+  //   return res;
+
+  // 第二次写，没想出来，抄了一遍
   let res = [],
     output = [];
-  for (num of nums) {
+  for (const num of nums) {
     output.push(num);
   }
   const backtrack = (n, first) => {
