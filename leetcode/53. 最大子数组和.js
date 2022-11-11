3b355ed2a99c25eb1c5ccf769c5ce246b0e35e91
LiChen <2446432344@qq.com>
2022-11-11 09:37:58 +0800

    lc 22-11-11

--- leetcode/53. 最大子数组和.js
+++ leetcode/53. 最大子数组和.js
@@ -20,15 +20,31 @@ var maxSubArray = function (nums) {
   // }
   // return maxSum;
   // 数组长度，dp初始化
-  const len = nums.length;
-  let dp = new Array(len).fill(0);
-  dp[0] = nums[0];
-  // 最大值初始化为dp[0]
-  let max = dp[0];
-  for (let i = 1; i < len; i++) {
-    dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
-    // 更新最大值
-    max = Math.max(max, dp[i]);
+  //   const len = nums.length;
+  //   let dp = new Array(len).fill(0);
+  //   dp[0] = nums[0];
+  //   // 最大值初始化为dp[0]
+  //   let max = dp[0];
+  //   for (let i = 1; i < len; i++) {
+  //     dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+  //     // 更新最大值
+  //     max = Math.max(max, dp[i]);
+  //   }
+  //     return max;
+
+  // 第二次写
+  let left = 0,
+    right = 0,
+    l = nums.length,
+    curMax = 0,
+    maxSum = -Infinity;
+  while (right !== l) {
+    if (curMax >= 0) {
+      curMax += nums[right++];
+      maxSum = Math.max(curMax, maxSum);
+    } else {
+      curMax -= nums[left++];
+    }
   }
-  return max;
+  return maxSum;
 };
