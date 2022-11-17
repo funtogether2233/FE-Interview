52fb7db51e351405eebec6061cbe8215826f122e
LiChen <2446432344@qq.com>
2022-11-17 11:12:19 +0800

    lc 22-11-17

--- leetcode/102. 二叉树的层序遍历.js
+++ leetcode/102. 二叉树的层序遍历.js
@@ -11,14 +11,37 @@
  * @return {number[][]}
  */
 var levelOrder = function (root) {
+  //   let res = [];
+  //   if (!root) {
+  //     return [];
+  //   }
+  //   let pre = [root];
+  //   while (pre.length !== 0) {
+  //     let level = [],
+  //       n = pre.length;
+  //     for (let i = 0; i < n; ++i) {
+  //       let node = pre.shift();
+  //       level.push(node.val);
+  //       if (node.left) {
+  //         pre.push(node.left);
+  //       }
+  //       if (node.right) {
+  //         pre.push(node.right);
+  //       }
+  //     }
+  //     res.push(level);
+  //   }
+  //     return res;
+
+  // 第二次写
   let res = [];
   if (!root) {
-    return [];
+    return res;
   }
   let pre = [root];
   while (pre.length !== 0) {
-    let level = [],
-      n = pre.length;
+    let n = pre.length,
+      level = [];
     for (let i = 0; i < n; ++i) {
       let node = pre.shift();
       level.push(node.val);
