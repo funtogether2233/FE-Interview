4066828782253822b6c4094192de56acde09117c
LiChen <2446432344@qq.com>
2022-11-21 10:03:20 +0800

    lc 22-11-21

--- leetcode/70. 爬楼梯.js
+++ leetcode/70. 爬楼梯.js
@@ -3,8 +3,15 @@
  * @return {number}
  */
 var climbStairs = function (n) {
+  //   let dp = [0, 1, 2];
+  //   for (let i = 3; i <= n; i++) {
+  //     dp[i] = dp[i - 1] + dp[i - 2];
+  //   }
+  //   return dp[n];
+
+  // 第二次写
   let dp = [0, 1, 2];
-  for (let i = 3; i <= n; i++) {
+  for (let i = 3; i <= n; ++i) {
     dp[i] = dp[i - 1] + dp[i - 2];
   }
   return dp[n];
