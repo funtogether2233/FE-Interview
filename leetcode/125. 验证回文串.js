9436242d628e1b54129684de213d2ab50252b6fa
LiChen <2446432344@qq.com>
2023-01-03 14:40:02 +0800

    lc 23-1-3

--- leetcode/125. 验证回文串.js
+++ leetcode/125. 验证回文串.js
@@ -3,8 +3,69 @@
  * @return {boolean}
  */
 var isPalindrome = function (s) {
-  let res = s.match(/[a-zA-Z0-9]/g);
-  return (
-    res?.toString().toUpperCase() === res?.reverse().toString().toUpperCase()
-  );
+  // 正则表达式
+  // let res = s.match(/[a-zA-Z0-9]/g);
+  // return res?.toString().toUpperCase()===res?.reverse().toString().toUpperCase();
+
+  // 杂鱼方法
+  // let res = [];
+  // let length = s.length;
+  // for (let i = 0; i < length; i++) {
+  //     let tmp = s[i].charCodeAt();
+  //     if ((tmp >= 48 && tmp <= 57) || (tmp>=65 && tmp <= 90) || (tmp >= 97 && tmp <= 122)) {
+  //         res.push(s[i].toUpperCase());
+  //     }
+  // }
+  // return res.toString() === res.reverse().toString();
+
+  // 双指针
+  // let res = [], length = s.length;
+  // for (let i = 0; i < length; i++) {
+  //     let tmp = s[i].charCodeAt();
+  //     if ((tmp >= 48 && tmp <= 57) || (tmp>=65 && tmp <= 90) || (tmp >= 97 && tmp <= 122)) {
+  //         res.push(s[i].toUpperCase());
+  //     }
+  // }
+  // length = res.length;
+  // let left = 0, right = length - 1;
+  // while (left < right) {
+  //     if (res[left] !== res[right]) {
+  //         return false;
+  //     }
+  //     ++left;
+  //     --right;
+  // }
+  // return true;
+
+  // 双指针优化
+  function isLetterOrDigit(c) {
+    let tmp = c.charCodeAt();
+    if (
+      (tmp >= 48 && tmp <= 57) ||
+      (tmp >= 65 && tmp <= 90) ||
+      (tmp >= 97 && tmp <= 122)
+    ) {
+      return true;
+    }
+    return false;
+  }
+  let left = 0,
+    right = s.length - 1;
+  while (left < right) {
+    while (left < right && !isLetterOrDigit(s[left])) {
+      ++left;
+    }
+    while (left < right && !isLetterOrDigit(s[right])) {
+      --right;
+    }
+    if (left >= right) {
+      break;
+    }
+    if (s[left].toUpperCase() !== s[right].toUpperCase()) {
+      return false;
+    }
+    ++left;
+    --right;
+  }
+  return true;
 };
--- leetcode/5. 最长回文子串.js
+++ leetcode/5. 最长回文子串.js
@@ -3,42 +3,42 @@
  * @return {string}
  */
 var longestPalindrome = function (s) {
-  // let dp =[s.length],
-  //     maxlength = 0, left = 0, right = 0;
-  // for(let i=0; i<s.length; i++){
-  //     dp[i] = [s.length];
-  // }
-  // for(let i = s.length - 1; i >= 0; i--){
-  //     for(let j = i; j < s.length; j++){
-  //         if(s[i] === s[j] && (j - i <= 1 || dp[i + 1][j-1])){
-  //             dp[i][j] = true;
-  //             if(j - i + 1 > maxlength){
-  //                 maxlength = j - i + 1;
-  //                 left = i;
-  //                 right =j;
-  //             }
-  //         }
+  // 动态规划
+  let length = s.length,
+    dp = new Array(length).fill().map(() => new Array(length)),
+    maxlength = 0,
+    begin = 0;
+  for (let i = 0; i < length; ++i) {
+    for (let j = i; j >= 0; --j) {
+      if (s[i] === s[j] && (i - j <= 1 || dp[i - 1][j + 1])) {
+        dp[i][j] = true;
+        if (i - j + 1 > maxlength) {
+          maxlength = i - j + 1;
+          begin = j;
+        }
+      }
+    }
+  }
+  return s.slice(begin, begin + maxlength);
+
+  // 中心扩展算法
+  // let start = 0, end = 0;
+  // for (let i = 0; i < s.length; i++) {
+  //     let len1 = expandAroundCenter(s, i, i),
+  //         len2 = expandAroundCenter(s, i, i + 1),
+  //         len = Math.max(len1, len2);
+  //     if (len > end - start) {
+  //         start = i - Math.floor((len - 1) / 2);
+  //         end = i + Math.floor(len / 2);
   //     }
   // }
-  // return s.slice(left, right+1);
+  // return s.slice(start, end + 1);
 
-  let maxlength = 0,
-    left = 0,
-    right = 0;
-  const extend = (s, i, j) => {
-    while (i >= 0 && j < s.length && s[i] === s[j]) {
-      if (j - i + 1 > maxlength) {
-        maxlength = j - i + 1;
-        left = i;
-        right = j;
-      }
-      i--;
-      j++;
-    }
-  };
-  for (let i = 0; i < s.length; i++) {
-    extend(s, i, i);
-    extend(s, i, i + 1);
-  }
-  return s.slice(left, right + 1);
+  // function expandAroundCenter(s, left, right) {
+  //     while (left >= 0 && right < s.length && s[left] === s[right]) {
+  //         --left;
+  //         ++right;
+  //     }
+  //     return right - left - 1;
+  // }
 };
