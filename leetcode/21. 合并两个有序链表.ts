b1dce8993226decf3a34314664d34dd92a34ee70
LiChen <2446432344@qq.com>
2023-01-05 14:38:19 +0800

    lc 23-1-5

---
+++ leetcode/21. 合并两个有序链表.ts
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+function mergeTwoLists(
+  list1: ListNode | null,
+  list2: ListNode | null
+): ListNode | null {
+  let res = new ListNode(),
+    p = res;
+  while (true) {
+    if (list1 === null) {
+      p.next = list2;
+      break;
+    }
+    if (list2 === null) {
+      p.next = list1;
+      break;
+    }
+    if (list1.val <= list2.val) {
+      p.next = list1;
+      p = p.next;
+      list1 = list1.next;
+      continue;
+    }
+    p.next = list2;
+    p = p.next;
+    list2 = list2.next;
+  }
+  return res.next;
+}
