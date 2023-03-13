ca76c4d56d8507fd85440c2b276cd82a2603025d
LiChen <2446432344@qq.com>
2023-03-13 13:41:30 +0800

    lc 23-3-13

--- questions/leetcode/349. 两个数组的交集.ts
+++ questions/leetcode/349. 两个数组的交集.ts
@@ -1,18 +1,27 @@
 function intersection(nums1: number[], nums2: number[]): number[] {
-  let res: number[] = [];
-  nums1.sort((a, b) => a - b);
-  nums2.sort((a, b) => a - b);
-  if (nums1.length !== 0 && nums2.length !== 0) {
-    if (nums2.includes(nums1[0])) {
-      res.push(nums1[0]);
+  // let res: number[] = [];
+  // nums1.sort((a, b) => a - b);
+  // nums2.sort((a, b) => a - b);
+  // if (nums1.length !== 0 && nums2.length !== 0) {
+  //   if (nums2.includes(nums1[0])) {
+  //     res.push(nums1[0]);
+  //   }
+  // }
+  // for (let i = 1; i < nums1.length; ++i) {
+  //   if (nums1[i] !== nums1[i - 1] && nums2.includes(nums1[i])) {
+  //     res.push(nums1[i]);
+  //   }
+  // }
+  // return res;
+
+  const hashSet = new Set(nums1),
+    res = new Set<number>();
+  for (const num of nums2) {
+    if (hashSet.has(num)) {
+      res.add(num);
     }
   }
-  for (let i = 1; i < nums1.length; ++i) {
-    if (nums1[i] !== nums1[i - 1] && nums2.includes(nums1[i])) {
-      res.push(nums1[i]);
-    }
-  }
-  return res;
+  return [...res];
 }
 
 export default void 0;
